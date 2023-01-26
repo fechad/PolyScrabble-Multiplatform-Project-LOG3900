5cf835e5233caf9d7c43e50a3668ea99d361b199
annagx <anna.guo@polymtl.ca>
2023-01-25 19:03:25 -0500

    added fn so app works on physical tablet -AG

--- client_leger/android/app/src/main/kotlin/com/polyapps/client_leger/MainActivity.kt
+++ client_leger/android/app/src/main/kotlin/com/polyapps/client_leger/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.polyapps.client_leger
 
 import io.flutter.embedding.android.FlutterActivity
-
+import android.os.Bundle
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 }
+
+
