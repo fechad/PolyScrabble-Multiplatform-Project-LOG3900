b1e5f7f019204bd049351aa9656f20267709469a
Etienne123456 <etienneaumais@gmail.com>
2023-02-14 15:54:12 +0000

    S04 114

---
+++ client/src/app/components/user-summary/user-summary.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserSummaryComponent } from './user-summary.component';
+
+describe('UserSummaryComponent', () => {
+    let component: UserSummaryComponent;
+    let fixture: ComponentFixture<UserSummaryComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UserSummaryComponent],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(UserSummaryComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
